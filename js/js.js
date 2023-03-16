15f406d4477087ac9ea3b249932008f46589f5cb
T3ru <juancabrera1293@gmail.com>
2023-03-16 11:19:11 -0500

    Subo Proyecto

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function VALLE(){
+    window.location.href = "VALLE.html";
+    }
+
+function CAUCA(){
+    window.location.href = "CAUCA.html";
+    }
+
+function NARIÑO(){
+    window.location.href = "NARIÑO.html";
+    }
